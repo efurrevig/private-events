53f805cc41ee13bd17da3633080a3161ec7935aa
Eric Furrevig <efurrevig@gmail.com>
2023-01-11 20:38:13 -0500

    Events index page, user model and login/logout

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_11_235229) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_12_000126) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.datetime "date"
+    t.string "location"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
