d49e9d4d4339420d97eb386009ed56af502152e9
Eric Furrevig <efurrevig@gmail.com>
2023-01-14 15:24:00 -0500

    added user-->event association and attendee arr to events

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root 'events#index'
 
   devise_for :users
-
+  resources :users do
+    resources :events
+  end
 end
---
+++ db/migrate/20230112063623_add_event_association_to_users.rb
@@ -0,0 +1,5 @@
+class AddEventAssociationToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :events, :user, index: true, foreign_key: true
+  end
+end
