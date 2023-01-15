15eaa5eb18ddb754efdbd700161cbacb9ba9c674
Eric Furrevig <efurrevig@gmail.com>
2023-01-14 22:46:09 -0500

    Added "new events" and can view events users own

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,22 @@ class EventsController < ApplicationController
     def index
         @events = Event.all
     end
+
+    def new
+        @event = current_user.events.new
+    end
+
+    def create
+        @event = current_user.events.build(event_params)
+        if @event.save
+            redirect_to root_path
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    private
+        def event_params
+            params.require(:event).permit(:title, :location, :description, :date)
+        end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_14_203453) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_14_220223) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_14_203453) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.integer "attendees", default: [], array: true
+    t.text "description"
+    t.string "title"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
